35dade39111483679c7085125fd3b35e03130d41
Faypo <king1532168@gmail.com>
2020-07-12 21:04:31 +0800

    [add] cat walk

---
+++ Assets/playerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+
+    public float jumpForce;
+    public float walkForce;
+    public float maxSpeed;
+
+    private void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+
+        int key = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        if(speedx < this.maxSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if(key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+
+        this.animator.speed = speedx / 2.0f;
+    }
+}
